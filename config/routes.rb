a9637b5e8c7e5824d39da3904135e46d7f43c71f
miohimemiya <capo_kaoru@yahoo.co.jp>
2015-01-05 00:44:48 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleApp::Application.routes.draw do
-   get "users/new"
+  # get "users/new"
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
 
+  resources :users
    root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
